fd4819271cbc2594fa33e8c8597a3a7217e9ff3a
zzaizzai <junsai0530@gmail.com>
2022-12-21 23:05:33 +0900

    init

---
+++ commmissions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CommmissionsConfig(AppConfig):
+    name = 'commmissions'
--- myapp/settings.py
+++ myapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.core.exceptions import ImproperlyConfigured
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -49,6 +51,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
+    'members'
+    'works'
+    'commissions'
 ]
 
 MIDDLEWARE = [
@@ -87,10 +92,15 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-work',
+        'USER': 'root',
+        'PASSWORD': get_env_variable('MYSQL_USER_PW'),
+        'HOST': get_env_variable('MYSQL_HOST'),
+        'PORT': get_env_variable('MYSQL_PORT'),
     }
 }
+
 
 
 # Password validation
